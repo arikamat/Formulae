6bd1b313e8a9335121d072eb12fa2ca4e4471296
Ari Kamat <arijkamat@gmail.com>
2020-03-11 17:47:01 -0400

    commited 2 files

---
+++ app/src/main/java/com/example/formulae/Displacement.java
@@ -0,0 +1,5 @@
+package com.example.formulae;
+
+public class Displacement {
+    //Ari
+}
---
+++ app/src/main/java/com/example/formulae/FMA.java
@@ -0,0 +1,20 @@
+package com.example.formulae;
+
+public class FMA {
+    private int cases;
+    //if try to find a case=3
+    // if try find m case =2
+    // if try to find case 1
+    private double f;
+    private double m;
+    private double a;
+
+    public FMA(double f, double m, double a, String find){
+        //if(find.equals(s))
+        this.f = f;
+        this.m=m;
+        this.a=a;
+    }
+
+    //Assigned to Sanket
+}
