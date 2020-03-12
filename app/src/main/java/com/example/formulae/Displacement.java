45c96699d50624889e9aea84ba0224adde289fbb
Ari Kamat <arijkamat@gmail.com>
2020-03-11 21:52:03 -0400

    Sample Code for F=ma has been created. Please use as reference for other java classes

--- app/src/main/java/com/example/formulae/Displacement.java
+++ app/src/main/java/com/example/formulae/Displacement.java
@@ -1,5 +1,5 @@
 package com.example.formulae;
 
 public class Displacement {
-    //Ari
+
 }
--- app/src/main/java/com/example/formulae/FMA.java
+++ app/src/main/java/com/example/formulae/FMA.java
@@ -5,16 +5,46 @@ public class FMA {
     //if try to find a case=3
     // if try find m case =2
     // if try to find case =1
-    private double f;
-    private double m;
-    private double a;
+    private double force;
+    private double mass;
+    private double acceleration;
 
     public FMA(double f, double m, double a, String find){
         //if(find.equals(s))
-        this.f = f;
-        this.m=m;
-        this.a=a;
+        force = f;
+        mass=m;
+        acceleration=a;
+        if(find.toLowerCase().equals("force")){
+            cases=1;
+        }
+        else if(find.toLowerCase().equals("force")){
+            cases=2;
+        }
+        else{
+            cases=3;
+        }
+        /*
+----------------------------------------------------------------------------------------
+         */
+        if(cases==1){
+            //User is looking for Force
+            force = mass*acceleration;
+        }
+        else if(cases==2){
+            mass = acceleration/force;
+        }
+        else{
+            acceleration = force/mass;
+        }
     }
 
-    //Assigned to Sanket
+    public double getForce(){
+        return force;
+    }
+    public double getMass(){
+        return mass;
+    }
+    public double getAcceleration(){
+        return acceleration;
+    }
 }
