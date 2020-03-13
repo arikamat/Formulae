bd1b52060d6f0db838352b1707fdeabf62eb1372
Ari Kamat <arijkamat@gmail.com>
2020-03-12 20:30:18 -0400

    done with v=gt

--- app/src/main/java/com/example/formulae/VGT.java
+++ app/src/main/java/com/example/formulae/VGT.java
@@ -4,27 +4,43 @@ public class VGT {
     //Freefall --> Velocity = gravity *time
     private double velocity;
     private double time;
-    private final double gravity=9.81;
+    private double gravity;
     private int cases;
     public VGT(double v, double t, String find){
-        if(find.toLowerCase().equals("velocity")){
-            cases=1;
-        }
-        else if(find.toLowerCase().equals("time")){
-            cases=2;
-        }
-        else{
-            cases=3;
-        }
+        //Constructor for if it is on earth
         velocity=v;
-        time=t;
+        time =t;
+        if(find.toLowerCase().equals("velocity")){ cases=1; }
+        else{ cases=2; }
+        switch(cases){
+            case 1:
+                velocity = gravity*time;
+                break;
+            case 2:
+                time = velocity/gravity;
+        }
+    }
+    public VGT(double v, double t, double g, String find){
+        //Constructor for other places
+        velocity=v;
+        time =t;
+        gravity =g;
+        if(find.toLowerCase().equals("velocity")){ cases=1; }
+        else{ cases=2; }
+        switch(cases){
+            case 1:
+                velocity = gravity*time;
+                break;
+            case 2:
+                time = velocity/gravity;
+        }
     }
     public double getVelocity(){
-        return gravity*time;
+        return velocity;
     }
 
     public double getTime() {
-        return velocity/gravity;
+        return time;
     }
 
 }
