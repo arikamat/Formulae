2d149f5ffd2cbdf6d6aefdceff060ff76adc28d9
Sanket Bhalotia <sanket.bhalotia@gmail.com>
2020-03-14 22:29:00 -0400

    i think i finished just check

---
+++ app/src/main/java/com/example/formulae/FreeFall.java
@@ -0,0 +1,92 @@
+package com.example.formulae;
+
+public class FreeFall<booelan> {
+
+    private double initVel;
+    private double finVel;
+    private double time;
+    private double timemaxh;
+    private double maxh;
+
+    private boolean initVelB = false;
+    private boolean finVelB = false;
+    private boolean timeB = false;
+    private boolean timemaxhB = false;
+    private boolean maxhB = false;
+
+    public void setInitVel(double initVel) {
+        this.initVel = initVel;
+        this.initVelB = true;
+    }
+
+    public void setFinVel(double finVel) {
+        this.finVel = finVel;
+        this.finVelB = true;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+        this.timeB = true;
+    }
+
+    public void setmaxh(double maxh) {
+        this.maxh = maxh;
+        this.maxhB = true;
+    }
+
+    public void setTimemaxh(double timemaxh) {
+        this.timemaxh = timemaxh;
+        this.timemaxhB = true;
+    }
+
+    public double[] find(String find){
+        double[] returnedVal = new double[2];
+        if(find.toLowerCase().equals("initial velocity")){
+            returnedVal[0]=getInitVel();
+            return returnedVal;
+        }
+        else if(find.toLowerCase().equals("time")){
+            returnedVal= getTime();
+            return returnedVal;
+        }
+        else if(find.toLowerCase().equals("final velocity")){
+            returnedVal[0]= getFinVel();
+            return returnedVal;
+        }
+        else if(find.toLowerCase().equals("time to max height")){
+            returnedVal[0]=getTimemaxh();
+            return returnedVal;
+        }
+        else{
+            returnedVal[0]= getMaxh();
+            return returnedVal;
+        }
+    }
+
+    public double getInitVel() {
+        if (maxhB){
+            return ( );
+            //somone fniish above return
+        }
+        else if (timemaxhB){
+            return (time * 9.8);
+        }
+    }
+
+    public double getFinVel() {
+        return (9.8 * time);
+    }
+
+    public double getTime() {
+        return (finVel / 9.8);
+    }
+
+    public double getMaxh() {
+        return ((initVel * initVel) / 19.6);
+    }
+
+    public double getTimemaxh() {
+        return (initVel / 9.8);
+
+    }
+}
