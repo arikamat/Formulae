f434d917a69f65d02af2789ab205587a6281be09
Ari Kamat <arijkamat@gmail.com>
2020-03-14 22:43:50 -0400

    Finish FreeFall Class

--- app/src/main/java/com/example/formulae/physics/FreeFall.java
+++ app/src/main/java/com/example/formulae/physics/FreeFall.java
@@ -1,19 +1,20 @@
 package com.example.formulae.physics;
 
-public class FreeFall<booelan> {
+public class FreeFall {
 
     private double initVel;
     private double finVel;
     private double time;
-    private double timemaxh;
-    private double maxh;
+    private double timeMaxH;
+    private double maxH;
+    private double gravity =9.81;  //m/s^2
+
 
     private boolean initVelB = false;
     private boolean finVelB = false;
     private boolean timeB = false;
-    private boolean timemaxhB = false;
-    private boolean maxhB = false;
-
+    private boolean timeMaxHB = false;
+    private boolean maxHB = false;
     public void setInitVel(double initVel) {
         this.initVel = initVel;
         this.initVelB = true;
@@ -29,14 +30,14 @@ public class FreeFall<booelan> {
         this.timeB = true;
     }
 
-    public void setmaxh(double maxh) {
-        this.maxh = maxh;
-        this.maxhB = true;
+    public void setMaxH(double maxH) {
+        this.maxH = maxH;
+        this.maxHB = true;
     }
 
-    public void setTimemaxh(double timemaxh) {
-        this.timemaxh = timemaxh;
-        this.timemaxhB = true;
+    public void setTimeMaxH(double timeMaxH) {
+        this.timeMaxH = timeMaxH;
+        this.timeMaxHB = true;
     }
 
     public double find(String find){
@@ -54,22 +55,31 @@ public class FreeFall<booelan> {
             return returnedVal;
         }
         else if(find.toLowerCase().equals("time to max height")){
-            returnedVal=getTimemaxh();
+            returnedVal= getTimeMaxH();
             return returnedVal;
         }
         else{
-            returnedVal= getMaxh();
+            returnedVal= getMaxH();
             return returnedVal;
         }
     }
 
     public double getInitVel() {
-        if (maxhB){
-            return ( );
-            //somone fniish above return
+        double insideSqrt;
+        if (maxHB){
+            insideSqrt=(2*gravity* maxH);
+            if(insideSqrt>=0){
+                return Math.sqrt(insideSqrt);
+            }
+            else{
+                return 0;
+            }
         }
-        else if (timemaxhB){
+        else if (timeMaxHB){
             return (time * 9.8);
+        }
+        else{
+            return 0;
         }
     }
 
@@ -81,11 +91,11 @@ public class FreeFall<booelan> {
         return (finVel / 9.8);
     }
 
-    public double getMaxh() {
+    public double getMaxH() {
         return ((initVel * initVel) / 19.6);
     }
 
-    public double getTimemaxh() {
+    public double getTimeMaxH() {
         return (initVel / 9.8);
 
     }
