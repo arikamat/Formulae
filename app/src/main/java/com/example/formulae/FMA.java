03a759b029067264071122fe218fde62ede0d33c
Sanket Bhalotia <sanket.bhalotia@gmail.com>
2020-03-14 21:44:32 -0400

    made one change

---
+++ app/src/main/java/com/example/formulae/FMA.java
@@ -0,0 +1,51 @@
+package com.example.formulae;
+
+public class FMA {
+    private double  acceleration, mass, force;
+
+
+    private boolean accelerationB=false;
+    private boolean massB = false;
+    private boolean forceB = false;
+
+
+    public void setMass(double mass){
+        this.mass =mass;
+        massB = true;
+    }
+
+    public void setForce(double force){
+        this.force =force;
+        forceB = true;
+    }
+
+    public void setAcceleration(double acceleration){
+        this.acceleration=acceleration;
+        accelerationB=true;
+    }
+
+    public double[] find(String find){
+        double[] returnedVal = new double[2];
+        if(find.toLowerCase().equals("mass")){
+            returnedVal[0]=getMass();
+            return returnedVal;
+        }
+
+        else if(find.toLowerCase().equals("force")){
+            returnedVal[0]= getForce();
+            return returnedVal;
+        }
+
+        else{
+            returnedVal[0]= getAcceleration();
+            return returnedVal;
+        }
+    }
+
+
+    public double getAcceleration(){
+
+    }
+
+}
+
