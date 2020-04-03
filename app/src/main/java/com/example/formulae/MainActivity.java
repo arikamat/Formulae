6997ea80bdcae38cad1db43be7799708c05637fe
Ari Kamat <arijkamat@gmail.com>
2020-04-03 19:28:50 -0400

    Added code to activity_main.xml Welcome is done
    Signed-off-by: Ari Kamat <arijkamat@gmail.com>

--- app/src/main/java/com/example/formulae/MainActivity.java
+++ app/src/main/java/com/example/formulae/MainActivity.java
@@ -2,20 +2,25 @@ package com.example.formulae;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 import com.example.formulae.physics.ConstantAcceleration;
 import com.example.formulae.physics.Displacement;
 import com.example.formulae.physics.FMA;
 
 public class MainActivity extends AppCompatActivity {
+
     private static Displacement displacement;
     private static ConstantAcceleration constAccel;
     private static FMA fma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
