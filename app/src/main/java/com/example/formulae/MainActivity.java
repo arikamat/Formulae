d0e62f7e5c2beb00759901664b16be9cb60919fb
Ari Kamat <arijkamat@gmail.com>
2020-04-04 14:48:37 -0400

    Main Activity finished Outline Code for Multiple Button Press fished Started code for physics 1 activity
    Signed-off-by: Ari Kamat <arijkamat@gmail.com>

--- app/src/main/java/com/example/formulae/MainActivity.java
+++ app/src/main/java/com/example/formulae/MainActivity.java
@@ -2,19 +2,24 @@ package com.example.formulae;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.formulae.physics.ConstantAcceleration;
 import com.example.formulae.physics.Displacement;
 import com.example.formulae.physics.FMA;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class MainActivity extends Activity implements View.OnClickListener {
+    private Button physicsBtn;
+    private Button chemistryBtn;
+    private Button engBtn;
     private static Displacement displacement;
     private static ConstantAcceleration constAccel;
     private static FMA fma;
@@ -22,6 +27,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        physicsBtn = (Button)findViewById(R.id.physicsbtn);
+        physicsBtn.setOnClickListener(this);
+        chemistryBtn = (Button) findViewById(R.id.chembtn);
+        engBtn=(Button)findViewById(R.id.engbtn);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.physicsbtn:
+                Intent intent=new Intent(getApplicationContext(),Physics1.class);
+                startActivityForResult(intent,0);
+                break;
+            case R.id.chembtn:
+                break;
+            case R.id.engbtn:
+                break;
+        }
     }
 }
