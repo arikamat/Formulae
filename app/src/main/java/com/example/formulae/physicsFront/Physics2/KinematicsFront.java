5682b6848ea3eb4977db1f04ae764966f4e5d38f
Sanket Bhalotia <sanket.bhalotia@gmail.com>
2020-04-07 19:15:52 -0400

    Elastic collisions ui done

--- app/src/main/java/com/example/formulae/physicsFront/Physics2/KinematicsFront.java
+++ app/src/main/java/com/example/formulae/physicsFront/Physics2/KinematicsFront.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 import com.example.formulae.R;
 
-public class kinematics extends AppCompatActivity {
+public class KinematicsFront extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
