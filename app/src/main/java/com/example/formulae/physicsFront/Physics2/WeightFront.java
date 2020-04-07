5e0b866c5b9783401c302ead08312e49095b94b6
Sanket Bhalotia <sanket.bhalotia@gmail.com>
2020-04-07 19:41:22 -0400

    weight ui done

---
+++ app/src/main/java/com/example/formulae/physicsFront/Physics2/WeightFront.java
@@ -0,0 +1,16 @@
+package com.example.formulae.physicsFront.Physics2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.formulae.R;
+
+public class WeightFront extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight_front);
+    }
+}
