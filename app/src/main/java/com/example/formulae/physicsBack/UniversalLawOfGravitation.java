a7b7c5ee28be786d0bc9c721583e99c897848eb9
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 19:34:36 -0400

    Make getter for UniversalLawOfGravitation

--- app/src/main/java/com/example/formulae/physicsBack/UniversalLawOfGravitation.java
+++ app/src/main/java/com/example/formulae/physicsBack/UniversalLawOfGravitation.java
@@ -2,7 +2,7 @@ package com.example.formulae.physicsBack;
 
 public class UniversalLawOfGravitation {
     private final double g = 6.67E-11;
-    private double mass1, mass2, distance, force, accelerationOfMass1, accelerationOfMass2;
+    private double mass1, mass2, distance, force,
 
     private boolean mass1B = false;
     private boolean mass2B = false;
@@ -29,5 +29,44 @@ public class UniversalLawOfGravitation {
         forceB = true;
     }
 
+    public UniversalLawOfGravitation(){};
 
+    private double getMass1() {
+        return (force*distance*distance)/(g*mass2);
+    }
+
+    private double getMass2() {
+        return (force*distance*distance)/(g*mass1);
+    }
+
+    private double getForce() {
+        return (g*mass2*mass1)/(distance*distance);
+    }
+
+    private double getDistance() {
+        return Math.sqrt((g*mass1*mass2)/(force));
+    }
+
+    private double getAccelerationOfMass1() {
+        return (g*mass2)/(distance*distance);
+    }
+
+    private double getAccelerationOfMass2() {
+        return (g*mass1)/(distance*distance);
+    }
+
+    public double find(String find){
+        if(find.toLowerCase().equals("mass1")){
+            return getMass1();
+        } else if(find.toLowerCase().equals("mass2")){
+            return getMass2();
+        } else if (find.toLowerCase().equals("distance")) {
+            return getDistance()
+        } else if (find.toLowerCase().equals("force")) {
+            return getForce();
+        } else if (find.toLowerCase().equals("accelerationOfMass1")) {
+            return getAccelerationOfMass1();
+        } else
+            return getAccelerationOfMass2();
+    }
 }
