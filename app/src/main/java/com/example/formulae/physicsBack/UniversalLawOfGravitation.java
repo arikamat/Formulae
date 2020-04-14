6aacd6083e88b2de5a36fa7e7a631b1e0b897fc9
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 19:18:15 -0400

    Create UniversalLawOfGravitation Class

---
+++ app/src/main/java/com/example/formulae/physicsBack/UniversalLawOfGravitation.java
@@ -0,0 +1,8 @@
+package com.example.formulae.physicsBack;
+
+public class UniversalLawOfGravitation {
+    private final double g = 6.67E-11;
+    private double mass1, mass2, distance, force, accelerationOfMass1, accelerationOfMass2;
+
+
+}
