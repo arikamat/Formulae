8ef2a2d90384f0709027b8a027c1d5fbad866651
michaelk522 <michaelkasprzak522@gmail.com>
2020-04-14 19:19:45 -0400

    Create all instance variables for UniversalLawOfGravitation

--- app/src/main/java/com/example/formulae/physicsBack/UniversalLawOfGravitation.java
+++ app/src/main/java/com/example/formulae/physicsBack/UniversalLawOfGravitation.java
@@ -4,5 +4,10 @@ public class UniversalLawOfGravitation {
     private final double g = 6.67E-11;
     private double mass1, mass2, distance, force, accelerationOfMass1, accelerationOfMass2;
 
+    private boolean mass1B = false;
+    private boolean mass2B = false;
+    private boolean distanceB = false;
+    private boolean forceB = false;
+
 
 }
